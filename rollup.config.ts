5baac91788c0aafdae06795804cc7bf084b1bbf8
charJKL <11600464+charJKL@users.noreply.github.com>
2025-09-17 17:04:49 +0200

    Reorganize structure. Change build process.

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,22 +1,39 @@
-/*export default 
-{   
-    input: "./addon.block.ts",
-    output:
-    {
-        file: "./build/background.js",
+import { RollupOptions } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
+import del from "rollup-plugin-delete";
 
-    }
-};
-*/
-import type { RollupOptions } from 'rollup';
-import typescript from '@rollup/plugin-typescript';
+
+// clear build folder from previous build
+const delConfig = 
+{
+    targets: ['./build/*']
+}	
+// copy icons, and manifest file
+const copyConfig = 
+{
+    targets: [ 
+        { src: "./src/icon/AddonBlockIcon*.png", dest:"./build" },
+        { src: "./src/manifest.json", dest:"./build" }
+    ]
+}
 
 export default
-{
-    input: "./src/addon.block.ts",
-    output: 
+[
     {
-        file: "./build/addon.block.js",
+        input: "./src/addon.block.ts",
+        output: 
+        {
+            file: "./build/addon.block.js",
+        },
+        plugins: [typescript(), del(delConfig), copy(copyConfig)]
     },
-    plugins: [typescript()]
-} satisfies RollupOptions;
+    {
+        input: "./src/public/settings.ts",
+        output: 
+        {
+            file: "./build/public/settings.js",
+        },
+        plugins: [typescript(), del(delConfig), copy(copyConfig)] 
+    }
+];
---
+++ src/public/settings.ts
@@ -0,0 +1,1 @@
+alert("Welcome on settings page");

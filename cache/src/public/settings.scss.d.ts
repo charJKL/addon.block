3fe7222433115c0596046d39b4432fbd794b9cfb
charJKL <11600464+charJKL@users.noreply.github.com>
2025-09-29 19:40:57 +0200

    Remove `cache` folder from git.

--- cache/src/public/settings.scss.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly "active": string;
-  readonly "header": string;
-  readonly "main": string;
-  readonly "nav": string;
-  readonly "navSettings": string;
-};
-export = styles;
-

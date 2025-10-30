81eb32e0c6a2063646ac3d81e7519aef1eeafced
charJKL <11600464+charJKL@users.noreply.github.com>
2025-10-30 10:06:00 +0100

    Fix issue with missing reference to `React`.

--- src/public/components/RelativeNavLink.tsx
+++ src/public/components/RelativeNavLink.tsx
@@ -2,5 +2,5 @@ import { NavLink, NavLinkProps } from "react-router";
 
 export function RelativeNavLink(props: NavLinkProps & React.RefAttributes<HTMLAnchorElement>)
 {
-    return <NavLink to={location.pathname + props.to} />
+    return <NavLink to={location.pathname + props.to} >{props.children}</NavLink>
 }
--- src/public/settings.tsx
+++ src/public/settings.tsx
@@ -3,5 +3,5 @@ import { BrowserRouter } from "react-router";
 
 import { App } from "./App";
 
-const root = createRoot(document.getElementById('app') as HTMLElement);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 	  root.render(<BrowserRouter><App/></BrowserRouter>);

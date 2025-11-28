7ed6ce09de7fba4e1f9c54543015e6ad03459127
charJKL <11600464+charJKL@users.noreply.github.com>
2025-11-28 08:40:06 +0100

    Implement `RulesService` basic feature.

--- src/addon.block.ts
+++ src/addon.block.ts
@@ -1,31 +1,62 @@
-import { RulesService as RulesServiceImpl } from "./service/RulesService";
+import { LocalStorage as LocalStorageApi } from "browser-extension-std/backend";
+import { NetRequest as NetRequestApi } from "browser-extension-std/backend";
+import { BackendComm as BackendCommApi } from "browser-extension-std/backend";
+
+import { RulesService as RulesServiceImpl} from "./service/RulesService";
+
+import type { Rule, RuleList, RuleDescId } from "./service/RulesService";
 
 
-const RulesService = new RulesServiceImpl();
+export type REST = 
+{
+	"GET://rules": () => RuleList;
+	"GET://rules/$id": (id: RuleDescId) => Rule;
+}
 
 
+const url = "./public/block.html";
+
+const LocalStorage = new LocalStorageApi<{}>({});
+const NetRequest = new NetRequestApi(url);
+const BackendComm = new BackendCommApi<REST>();
 
 
+const rulesService = new RulesServiceImpl(LocalStorage, NetRequest);
+
+BackendComm.addEndpointListener("GET://rules", function()
+{
+	console.log("Addon.block.ts::GET://rules");
+	return [];
+});
+
+BackendComm.addEndpointListener("GET://rules/$id", function(id: RuleDescId)
+{
+	console.log("Addon.block.ts::GET://rules/$id");
+	return {id: "asdsad", netRequestId: 23, addedTime: 234, regexp: "/asdasd/"};
+})
+
+
+/**
+ * Everything below is for testing purposes
+ */
+
+const blockPagePath = browser.runtime.getURL("./block.html");
+console.log(blockPagePath);
 type NetRequestRule = browser.declarativeNetRequest.Rule;
+const ruleAction = { type: "redirect", redirect: { extensionPath: "/block.html" } };
+//const ruleAction = { type: "redirect", redirect: { url: "https://stackoverflow.com/" } };
 
-const rule : NetRequestRule = {id: 1, action: { type: "block" },  condition: {urlFilter: "pomofocus",  resourceTypes: ["main_frame", "sub_frame"]}};
+const ruleCondition = {urlFilter: "||pomofocus",  resourceTypes: ["main_frame", "sub_frame"] };
+const rule : NetRequestRule = {id: 1, condition: ruleCondition, action: ruleAction };
 const addRules : NetRequestRule[] = [rule];
 const removeRuleIds = [1];
 let rulesUpdated = browser.declarativeNetRequest.updateDynamicRules({addRules, removeRuleIds});
 
 console.log(rulesUpdated);
-
-
 browser.tabs.create({url: "/settings.html"});
 
 
-
-
-
-browser.runtime.onMessage.addListener(function(message, sender){
-    console.log("message from frontpage", message, sender);
-});
-
+browser.declarativeNetRequest.getDynamicRules().then(rules => console.log(rules));
 
 
 
--- src/service/RulesService.ts
+++ src/service/RulesService.ts
@@ -1,53 +1,77 @@
+import { LocalStorage } from "browser-extension-std/backend";
+import { NetRequest } from "browser-extension-std/backend";
 
 
-
-// Types:
 export type UniqueIdString = string;
 export type UniqueIdNumber = number;
 export type Timestamp = number;
 export type Rule = 
 {
-    id: UniqueIdString;
-    netRequestId: UniqueIdNumber;
-    addedTime: Timestamp;
-    regexp: string;
+	id: UniqueIdString;
+	netRequestId: UniqueIdNumber;
+	addedTime: Timestamp;
+	regexp: string;
 }
 export type RuleList = Array<Rule>;
 export type RuleDesc = Pick<Rule, "regexp">;
 export type RuleDescId = Pick<Rule, "id"> | Pick<Rule, "netRequestId"> | UniqueIdString;
 export type RuleDeleted = { id: null, netRequestId: null }; // TODO base this on Rule type like others 
+export type RulesStorageBlueprint = { rules: RuleList };
 
 /**
  * IRulesService 
  */
 interface IRulesService
 {
-    getRules(): RuleList;
-    addRule(ruleDesc: RuleDesc) : Rule;
-    removeRule(ruleDescId: RuleDescId): RuleDeleted;
+	getRules(): RuleList;
+	addRule(ruleDesc: RuleDesc) : Promise<Rule>;
+	removeRule(ruleDescId: RuleDescId): RuleDeleted;
 }
 
 export class RulesService implements IRulesService
 {
-    
-    getRules() : RuleList 
-    {
-        // TODO query local storage for rules 
-        return [];
-    }
-    
-    addRule(ruleDesc: RuleDesc) : Rule
-    {
-        // TODO Add to database
-        // TODO Add to netRequest
-        const rule = { id: "", netRequestId: 0, addedTime: 0, regexp: ruleDesc.regexp }; 
-        return rule;
-    }
+	private readonly $storage: LocalStorage<RulesStorageBlueprint>;
+	private readonly $netRequest: NetRequest;
 
-    removeRule(ruleDescId: RuleDescId ): RuleDeleted
-    {
-        const rule = { id: null, netRequestId: null, addedTime: 0, regexp: ""}; 
-        return rule;
-    }
+	public constructor(storage: LocalStorage<{}>, netRequest: NetRequest)
+	{
+		const storageDefaultValue : RulesStorageBlueprint = { rules: [] };
+		this.$storage = storage.init(storageDefaultValue);
+		this.$netRequest = netRequest;
+	}
 
+	public getRules() : RuleList 
+	{
+		// TODO query local storage for rules 
+		return [];
+	}
+	
+	public async addRule(ruleDesc: RuleDesc) : Promise<Rule>
+	{
+		const getTimestamp = () => Math.floor(Date.now() / 1000); // TODO move this somewhere
+		const generateId = () => Date.now().toString(36) + Math.random().toString(36).substring(2);
+		const rules = await this.$storage.get("rules");
+
+		const notRegisteredRule = 
+		{
+			id: generateId(), // TODO make sure this id is unique in list
+			netRequestId: null,
+			addedTime: getTimestamp(),
+			regexp: ruleDesc.regexp
+		}
+
+		const netRequestRuleDesc = { regexp: new RegExp(ruleDesc.regexp) }
+		const result = await this.$netRequest.addDynamicRule(netRequestRuleDesc);
+
+		const rule = { ...notRegisteredRule, netRequestId: 23 };
+		rules.push(rule);
+		const save = await this.$storage.set("rules", rules);
+		return rule;
+	}
+
+	public removeRule(ruleDescId: RuleDescId ): RuleDeleted
+	{
+		const rule = { id: null, netRequestId: null, addedTime: 0, regexp: ""}; 
+		return rule;
+	}
 }
